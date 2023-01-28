ee7cf6d25bd6082e76384b8f10d2c3df5fb930d4
LeannF <leann.farant@outlook.fr>
2023-01-28 04:08:38 +0100

    TU

--- TU Challenge/MyMathImplementation.cs
+++ TU Challenge/MyMathImplementation.cs
@@ -8,6 +8,135 @@ namespace TU_Challenge
 {
     public class MyMathImplementation
     {
-        
+        public static int IsInOrderDesc(int a, int b)
+        {
+            if (a > b)
+            {
+                return 1;
+            }
+            else if (a < b)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static bool IsMajeur(int age)
+        {
+            if (age <0 || age>140)
+            {
+                throw new NotImplementedException("abuse pas frerot");
+            }
+            
+
+            if (age>=18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool IsEven(int a)
+        {
+            if (a%2==0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        public static bool IsDivisible(int a, int b)
+        {
+            return (a % b == 0);
+
+        }
+        public static bool IsPrimary(int a)
+        {
+            int num = 0;
+            for (int i = 0; i <= a; i++)
+            {
+                num++;
+            }
+            throw new NotImplementedException();
+        }
+        public static int Power(int a, int b)
+        {
+            int p = a;
+            for (int i = 0; i < b; i++)
+            {
+                p = p * a;
+            }
+            return p;
+        }
+
+        public static int Power2(int a)
+        {
+            return a * a;
+        }
+
+        public static List<int> GetAllPrimary(int a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int IsInOrder(int a, int b)
+        {
+            if (a < b)
+            {
+                return 1;
+            }
+            else if (a > b)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool IsListInOrder(List<int> list)
+        {
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static List<int> Sort(List<int> list)
+        {
+            List<int> sortedList = new List<int>();
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                if (list[i] < list[i + 1])
+                {
+                    sortedList.Add(list[i]);    
+                    list.RemoveAt(i);
+                    i = 0;
+                }
+            }
+            return sortedList;
+        }
+
+        /*public static List<int> GenericSort(List<int> toSort, Func<int, int, int> isInOrder)
+        {
+            throw new NotImplementedException();
+        }*/
     }
 }
--- TU Challenge/MyStringImplementation.cs
+++ TU Challenge/MyStringImplementation.cs
@@ -8,6 +8,100 @@ namespace TU_Challenge
 {
     public class MyStringImplementation
     {
-        
+        public static string BazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsNullEmptyOrWhiteSpace(string a)
+        {
+            if (a == null || a.Length <1)
+            {
+                return true;
+
+            }
+            else
+            {
+                foreach (char item in a)
+                {
+                    if (item != ' ') 
+                        return false;
+                }
+                return true;
+            }
+        }
+        public static string MixString(string a, string b)
+        {
+            if (a == null || a.Length <= 0)
+            {
+                throw new ArgumentException(nameof(a));
+            }
+            else if (b == null || b.Length <= 0)
+            {
+                throw new ArgumentException(nameof(b));
+            }
+            string? c = "";
+
+            for (int i = 0; i < a.Length + b.Length; i++)
+            {
+                if (i < a.Length)
+                {
+                    c += a[i];
+                }
+
+                if (i < b.Length)
+                {
+                    c += b[i];
+                }
+            }
+            return c;
+        }
+
+        public static string Reverse(string a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ToLowerCase(string a)
+        {
+            string? str = "";
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                char c = a[i];
+
+                if (c != ' ' && c < 97 && c > 64)
+                {
+                    c += (char)32;
+                }
+                else if (c == 'É')
+                {
+                    c = 'é';
+                }
+                str += c;
+            }
+            return str;
+        }
+
+        public static string UnBazardString(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Voyelles(string a)
+        {
+            a = ToLowerCase(a);
+            string? str = "";
+            char[] voyelles = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+            foreach (char c in a)
+            {
+                if (voyelles.Contains(c) && !str.Contains(c))
+                {
+                    str += c;
+                }
+            }
+            return str;
+        }
     }
 }
